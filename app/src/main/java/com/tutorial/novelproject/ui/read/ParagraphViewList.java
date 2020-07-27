70b83ef0c52586696db345b78b65549cf4716ce9
duydd25031999 <doducduy.vietnam@gmail.com>
2020-07-27 13:12:24 +0700

    get image url from chapter context

--- app/src/main/java/com/tutorial/novelproject/ui/read/ParagraphViewList.java
+++ app/src/main/java/com/tutorial/novelproject/ui/read/ParagraphViewList.java
@@ -2,18 +2,25 @@ package com.tutorial.novelproject.ui.read;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.tutorial.novelproject.R;
 import com.tutorial.novelproject.model.ChapterContent;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ParagraphViewList {
     private LinearLayout layout;
     private Context context;
     private LayoutInflater layoutInflater;
+    private static final Pattern pattern = Pattern.compile("^--img--\\[(.*)\\]");
 
     public ParagraphViewList(LinearLayout layout, Context context) {
         this.layout = layout;
@@ -21,17 +28,31 @@ public class ParagraphViewList {
         this.layoutInflater = LayoutInflater.from(context);
     }
 
-    private TextView createView(String content) {
-        TextView view = (TextView) layoutInflater.inflate(R.layout.paragraph_item, layout, false);
-        view.setText(content);
+    private View createView(String content) {
+        Matcher matcher = pattern.matcher(content);
+        if (matcher.find()) {
+            String imageUrl = matcher.group(1);
+            ImageView imageView = (ImageView) layoutInflater.inflate(R.layout.paragragh_image, layout, false);
+            Glide
+                .with(context)
+                .load(imageUrl)
+                .centerCrop()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .placeholder(R.drawable.ic_baseline_sync_24)
+                .dontTransform()
+                .into(imageView);
+            return imageView;
+        }
+        TextView textView = (TextView) layoutInflater.inflate(R.layout.paragraph_item, layout, false);
+        textView.setText(content);
 
-        return view;
+        return textView;
     }
 
     public void listView(List<ChapterContent> contents) {
         layout.removeAllViews();
         for (ChapterContent content : contents) {
-            TextView view = createView(content.getContent());
+            View view = createView(content.getContent());
             layout.addView(view);
         }
     }
